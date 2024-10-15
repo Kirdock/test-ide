12da85eb6972a46446ed03895aeda7975ff97aa5
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2024-10-15 17:37:10 +0200

    tsconfig spec setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+export {}
+// Disabling it because this also imports jest types which interferes with the tsconfig
+// import '@testing-library/jest-dom';
